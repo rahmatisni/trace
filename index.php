c124b6fdcfac932bb314d8af3232787ff132196e
rahmatisni@gmail.com <rahmatisni@gmail.com>
2024-07-15 13:58:24 +0700

    Exc Debug IN CI3

--- index.php
+++ index.php
@@ -69,43 +69,69 @@ require_once('vendor/autoload.php'); // Sesuaikan dengan lokasi autoload.php di 
  * Different environments will require different levels of error reporting.
  * By default development will show errors but testing and live will hide them.
  */
-// switch (ENVIRONMENT)
-switch (ENVIRONMENT)
 
-{
-	case 'development':
-		error_reporting(-1);
-		ini_set('display_errors', 0);
-		ini_set('display_errors','off');
-	break;
+ switch (ENVIRONMENT)
+ {
+	 case 'development':
+		 error_reporting(-1);
+		 ini_set('display_errors', 1);
+	 break;
+ 
+	 case 'testing':
+	 case 'production':
+		 ini_set('display_errors', 0);
+		 if (version_compare(PHP_VERSION, '5.3', '>='))
+		 {
+			 error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED & ~E_STRICT & ~E_USER_NOTICE & ~E_USER_DEPRECATED);
+		 }
+		 else
+		 {
+			 error_reporting(E_ALL & ~E_NOTICE & ~E_STRICT & ~E_USER_NOTICE);
+		 }
+	 break;
+ 
+	 default:
+		 header('HTTP/1.1 503 Service Unavailable.', TRUE, 503);
+		 echo 'The application environment is not set correctly.';
+		 exit(1); // EXIT_ERROR
+ }
 
-	case 'testing':
-		error_reporting(0);
+//  switch (ENVIRONMENT)
 
-		ini_set('display_errors', 0);
-		ini_set('display_errors','off');
-	case 'production':
-		error_reporting(0);
-		ini_set('display_errors', 0);
-		ini_set('display_errors','off');
+// {
+// 	case 'development':
+// 		error_reporting(-1);
+// 		ini_set('display_errors', 0);
+// 		ini_set('display_errors','off');
+// 	break;
 
-		// if (version_compare(PHP_VERSION, '5.3', '>='))
-		// {
-		// 	error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED & ~E_STRICT & ~E_USER_NOTICE & ~E_USER_DEPRECATED);
-		// }
-		// else
-		// {
-		// 	error_reporting(E_ALL & ~E_NOTICE & ~E_STRICT & ~E_USER_NOTICE);
-		// }
-	break;
+// 	case 'testing':
+// 		error_reporting(0);
 
-	default:
-		ini_set('display_errors', 0);
-		ini_set('display_errors','off');
-		header('HTTP/1.1 503 Service Unavailable.', TRUE, 503);
-		echo 'The application environment is not set correctly.';
-		exit(1); // EXIT_ERROR
-}
+// 		ini_set('display_errors', 0);
+// 		ini_set('display_errors','off');
+// 	case 'production':
+// 		error_reporting(0);
+// 		ini_set('display_errors', 0);
+// 		ini_set('display_errors','off');
+
+// 		// if (version_compare(PHP_VERSION, '5.3', '>='))
+// 		// {
+// 		// 	error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED & ~E_STRICT & ~E_USER_NOTICE & ~E_USER_DEPRECATED);
+// 		// }
+// 		// else
+// 		// {
+// 		// 	error_reporting(E_ALL & ~E_NOTICE & ~E_STRICT & ~E_USER_NOTICE);
+// 		// }
+// 	break;
+
+// 	default:
+// 		ini_set('display_errors', 0);
+// 		ini_set('display_errors','off');
+// 		header('HTTP/1.1 503 Service Unavailable.', TRUE, 503);
+// 		echo 'The application environment is not set correctly.';
+// 		exit(1); // EXIT_ERROR
+// }
 
 /*
  *---------------------------------------------------------------
