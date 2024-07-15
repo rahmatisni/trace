eaf3e890e506bd2899a86b00618efae35ac33fc7
rahmatisni@gmail.com <rahmatisni@gmail.com>
2024-07-15 14:05:06 +0700

    Exc Debug IN CI3

--- index.php
+++ index.php
@@ -78,14 +78,21 @@ require_once('vendor/autoload.php'); // Sesuaikan dengan lokasi autoload.php di 
  
 	 case 'testing':
 	 case 'production':
+		
 		 ini_set('display_errors', 0);
 		 if (version_compare(PHP_VERSION, '5.3', '>='))
 		 {
-			 error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED & ~E_STRICT & ~E_USER_NOTICE & ~E_USER_DEPRECATED);
+			error_reporting(0);
+            // ini_set('display_errors', 0);  
+
+			//  error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED & ~E_STRICT & ~E_USER_NOTICE & ~E_USER_DEPRECATED);
 		 }
 		 else
 		 {
-			 error_reporting(E_ALL & ~E_NOTICE & ~E_STRICT & ~E_USER_NOTICE);
+			error_reporting(0);
+            // ini_set('display_errors', 0);  
+
+			//  error_reporting(E_ALL & ~E_NOTICE & ~E_STRICT & ~E_USER_NOTICE);
 		 }
 	 break;
  
