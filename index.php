2e4b1b35891ad87ece5eba908ec04525344426e2
rahmatisni@gmail.com <rahmatisni@gmail.com>
2024-07-15 13:36:32 +0700

    Exc Debug IN CI3

--- index.php
+++ index.php
@@ -71,17 +71,21 @@ switch (ENVIRONMENT)
 	break;
 
 	case 'testing':
+	
 	case 'production':
-		ini_set('display_errors', 0);
-		if (version_compare(PHP_VERSION, '5.3', '>='))
-		{
-			error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED & ~E_STRICT & ~E_USER_NOTICE & ~E_USER_DEPRECATED);
-		}
-		else
-		{
-			error_reporting(E_ALL & ~E_NOTICE & ~E_STRICT & ~E_USER_NOTICE);
-		}
+		error_reporting(0);
 	break;
+	// case 'production':
+	// 	ini_set('display_errors', 0);
+	// 	if (version_compare(PHP_VERSION, '5.3', '>='))
+	// 	{
+	// 		error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED & ~E_STRICT & ~E_USER_NOTICE & ~E_USER_DEPRECATED);
+	// 	}
+	// 	else
+	// 	{
+	// 		error_reporting(E_ALL & ~E_NOTICE & ~E_STRICT & ~E_USER_NOTICE);
+	// 	}
+	// break;
 
 	default:
 		header('HTTP/1.1 503 Service Unavailable.', TRUE, 503);
